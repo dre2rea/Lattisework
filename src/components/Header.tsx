6ee904b95ec026c3b37d17d22081a7ce36fa12f1
Wonjae <braomli@gmail.com>
2026-02-17 20:34:53 +0900

    next fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,15 @@ import { Logo } from './Logo'
 import { SearchBar } from './SearchBar'
 
 interface HeaderProps {
+  searchQuery?: string
   onSearchSubmit?: (query: string) => void
 }
 
-export function Header({ onSearchSubmit }: HeaderProps) {
+export function Header({ searchQuery, onSearchSubmit }: HeaderProps) {
   return (
-    <header className="flex flex-col gap-5 py-6 md:flex-row md:items-center md:justify-between md:py-[30px]">
+    <header className="flex flex-col gap-5 py-6 md:flex-row md:items-center md:justify-between md:py-[20px]">
       <Logo />
-      <SearchBar onSearchSubmit={onSearchSubmit} />
+      <SearchBar searchQuery={searchQuery} onSearchSubmit={onSearchSubmit} />
     </header>
   )
 }
